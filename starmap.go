2f981609d316f6de046fe4b9f8535cd82146fae2
Hugh Wimberly <hugh.wimberly@gmail.com>
2013-06-23 15:17:16 -0700

    Moved to library; added tests.

--- starmap.go
+++ starmap.go
@@ -1,16 +1,26 @@
-package main
+package starmap
 
 import (
-	"fmt"
 	"encoding/csv"
 	"os"
 	"strconv"
+	"math"
+	"io"
 )
 
 type Star struct {
 	Id int
 	Name, Class string
 	X, Y, Z, Magnitude float64
+}
+
+func (s *Star) squaredDistance(other *Star) float64 {
+	xd, yd, zd := s.X - other.X, s.Y - other.Y, s.Z - other.Z
+	return xd*xd + yd*yd + zd*zd
+}
+
+func (s *Star) Distance(other *Star) float64 {
+	return math.Pow(s.squaredDistance(other), 0.5)
 }
 
 func atof(s string) (float64, error) { return strconv.ParseFloat(s, 64) }
@@ -28,22 +38,23 @@ func newStar(s []string) (*Star, error) {
 	return star, nil
 }
 
-func readFromFile(f string) (err error) {
-	reader, err := os.Open(f)
-	if err != nil { return err }
-	defer reader.Close()
+func readFromCSV(reader io.Reader) ([]*Star, error) {
 	r := csv.NewReader(reader)
+	var stars []*Star
+	var err error
 	for record, err := r.Read(); err == nil; record, err = r.Read() {
-		if record[2] != "" {
-			star, _ := newStar(record)
-			fmt.Println(star)
-		}
+		if record[2] != "1" { continue }
+		if star, err := newStar(record); err == nil {
+			stars = append(stars, star)
+		} else { return nil, err }
 	}
-	return err
+	if err == nil { return stars, nil }
+	return nil, err
 }
 
-func main() {
-	readFromFile("HabHYG.csv")
-
-	return
+func readFromFile(f string) ([]*Star, error) {
+	reader, err := os.Open(f)
+	if err != nil { return nil, err }
+	defer reader.Close()
+	return readFromCSV(reader)
 }
